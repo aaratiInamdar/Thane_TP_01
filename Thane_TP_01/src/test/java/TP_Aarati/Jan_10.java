f89df851f9ce095921ee212c2bbb35c686609abd
Abhishek <Abhishek@DESKTOP-E9KPRUH>
2026-01-14 16:09:50 +0530

    add name

--- Thane_TP_01/src/test/java/TP_Aarati/Jan_10.java
+++ Thane_TP_01/src/test/java/TP_Aarati/Jan_10.java
@@ -4,4 +4,6 @@ public class Jan_10 {
 	public static void main(String[] args) {
 		System.out.println("hii");
 	}
+	
+	
 }
---
+++ Thane_TP_01/src/test/java/TP_Aarati/githubtest.java
@@ -0,0 +1,16 @@
+package TP_Aarati;
+
+import org.testng.annotations.Test;
+
+public class githubtest {
+	
+  @Test
+  
+  public void nametest()
+  {
+	  System.out.println("abhi");
+	  
+	  
+  }
+
+}
