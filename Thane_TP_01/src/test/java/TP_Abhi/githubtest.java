632948ecf081c2dda991759dc0e471f6158fc4fd
Abhishek <Abhishek@DESKTOP-E9KPRUH>
2026-01-15 10:50:29 +0530

    change package

---
+++ Thane_TP_01/src/test/java/TP_Abhi/githubtest.java
@@ -0,0 +1,16 @@
+package TP_Abhi;
+
+import org.testng.annotations.Test;
+
+public class githubtest {
+	
+  @Test
+  
+  public void nametest()
+  {
+	  System.out.println("abhi");
+	  
+	  
+  }
+
+}
