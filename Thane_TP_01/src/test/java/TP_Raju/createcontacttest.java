b3137f9fb88164f26521dda43c4968bc2f66c5af
rajuwaykar6-netizen <rajuwaykar6@gmail.com>
2026-01-16 16:18:38 +0530

    2 classes added

---
+++ Thane_TP_01/src/test/java/TP_Raju/createcontacttest.java
@@ -0,0 +1,21 @@
+package TP_Raju;
+
+import org.testng.annotations.Test;
+
+public class createcontacttest {
+
+	@Test
+	public void createcontact() {
+
+		System.out.println("create contact");
+
+	}
+
+	@Test
+	public void verifycontact() {
+
+		System.out.println("verify contact");
+
+	}
+
+}
---
+++ Thane_TP_01/src/test/java/TP_Raju/createorgtest.java
@@ -0,0 +1,21 @@
+package TP_Raju;
+
+import org.testng.annotations.Test;
+
+public class createorgtest {
+
+	@Test
+	public void createorg() {
+
+		System.out.println("create org");
+
+	}
+
+	@Test
+	public void verifyorg() {
+
+		System.out.println("verify org");
+
+	}
+
+}
